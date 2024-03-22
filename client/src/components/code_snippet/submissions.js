2a263e6e924907a3e7aaeefe0f47c9c1c28c88d4
shaunak-09 <shaunakghosh09@kgpian.iitkgp.ac.in>
2024-03-22 15:53:31 +0530

    .

--- client/src/components/code_snippet/submissions.js
+++ client/src/components/code_snippet/submissions.js
@@ -48,7 +48,7 @@ const Page2 = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {submissions.length!=0 &&submissions?.map((submission, ind)=>(
+                        {submissions?.length!=0 &&submissions?.map((submission, ind)=>(
                             <>
                             <tr key={`sub${ind}`} class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
